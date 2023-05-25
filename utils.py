e5a5b89a369c12793a3d9e1e35850884220d672e
justvlade <justvlade@mail.ru>
2023-05-25 23:20:08 +0300

    Update utils.py
    Code rewritten

--- utils.py
+++ utils.py
@@ -1,30 +1,23 @@
+#!/usr/bin/python3
 import typing
 import random as rnd
-
 
 def get_restriction_time(string: str) -> typing.Optional[int]:
     """
     Get user restriction time in seconds
-
     :string: string to check for multiplier. The last symbol should be one of:
         "m" for minutes, "h" for hours and "d" for days
     :return: number of seconds to restrict or None if error
     """
-    if string == "lucky": # randomised mute time 4 hours max
-        string = str(rnd.randint(1, 240)) + "m"
-    if len(string) < 2:
-        return None
-    letter = string[-1]
-    try:
-        number = int("".join(c for c in string if c.isdecimal()))
-    except TypeError:
-        return None
-    else:
-        if letter == "m":
-            return 60 * number
-        elif letter == "h":
-            return 3600 * number
-        elif letter == "d":
-            return 86400 * number
-        else:
-            return None
+    suffix={ #suffixes which can be converted to seconds are here
+        'm': 60,
+        'h': 3600,
+        'd': 86400
+    }
+    string=string.replace(' ','') # remove spaces
+    if string=='lucky':  # randomised mute time 4 hours max
+        return rnd.randint(1, 240) * 60 # return random seconds immediately
+    number, letter = string[:-1], string[-1:] # separation of number and suffix
+    if number.isdigit() and letter in suffix: # if number contains only digits and letter can be converted to seconds
+        return int(number) * suffix[letter] # convert number to seconds
+    return None # if string is not correct we come here and return None
